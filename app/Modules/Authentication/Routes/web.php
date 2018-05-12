c515a9b78bb860225b69364b56697a02d1b718bd
Hadi Firmansyah <hadifirmansyahh@gmail.com>
2018-05-12 14:49:10 +0700

    add Auth Module

---
+++ app/Modules/Authentication/Routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your module. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+    Route::name('index')->get('/', function () {
+        dd('This is the Authentication module index page. Build something great!');
+    });
+    // Register Route
+    Route::name('register')->get('/register', 'RegisterController@register');
+    Route::name('register.post')->post('/register', 'RegisterController@postRegister');
+    // Login Route
+    Route::name('login')->get('/login', 'LoginController@login');
+    Route::name('login.post')->post('/login', 'LoginController@postLogin');
+});
--- config/app.php
+++ config/app.php
@@ -164,6 +164,8 @@ return [
          * Additional Service Providers...
          */
         Caffeinated\Modules\ModulesServiceProvider::class,
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -214,6 +216,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Module' => Caffeinated\Modules\Facades\Module::class,
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
