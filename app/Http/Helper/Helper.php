2dcb00befe4c49b583b1f5f5701687c90ea9dadb
Hadi Firmansyah <hadifirmansyahh@gmail.com>
2018-08-06 06:05:47 +0700

    add general helper

---
+++ app/Http/Helper/Helper.php
@@ -0,0 +1,44 @@
+<?php
+use Illuminate\Support\Debug\Dumper;
+
+function dr(...$args)
+{
+    foreach ($args as $key => $arg) {
+        if ($arg instanceof Illuminate\Contracts\Support\Arrayable) {
+            $args[$key] = $arg->toArray();
+        }
+    }
+
+    foreach ($args as $key => $x) {
+        (new Dumper)->dump($x);
+    }
+}
+
+function ddr()
+{
+    $args = func_get_args();
+    foreach ($args as $key => $arg) {
+        if ($arg instanceof Illuminate\Contracts\Support\Arrayable) {
+            $args[$key] = $arg->toArray();
+        }
+    }
+    call_user_func_array('dd', $args);
+}
+
+if (! function_exists('roles')) {
+    function roles()
+    {
+        $roles = \Sentinel::getUser()->roles()->first()->slug;
+
+        return strtolower($roles);
+    }
+}
+
+if (! function_exists('user')) {
+    function user()
+    {
+        $user = \Sentinel::getUser();
+
+        return $user;
+    }
+}
