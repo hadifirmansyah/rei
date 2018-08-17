b19b89c32bf1f0eb644e16ce3a76f3e5b043df7b
Hadi Firmansyah <hadifirmansyahh@gmail.com>
2018-08-18 03:22:05 +0700

    update dashboard

--- app/Modules/Admin/Http/Controllers/HomeController.php
+++ app/Modules/Admin/Http/Controllers/HomeController.php
@@ -5,6 +5,10 @@ namespace App\Modules\Admin\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Purchasing;
+use App\Modules\Authentication\Models\User;
+use DB;
 
 class HomeController extends Controller
 {
@@ -15,6 +19,16 @@ class HomeController extends Controller
      **/
     public function index()
     {
-        return view('admin::general.dashboard');
+        $data['count']['products'] = Product::count();
+        $data['count']['purchasing'] = Purchasing::count();
+        $data['count']['users'] = User::count();
+
+        $data['monthly'] = Purchasing::selectRaw('MONTHNAME(created_at) as month, COUNT(*) as count')
+                            ->groupBy(DB::raw('MONTHNAME(created_at)'))
+                            ->get();
+
+        return view('admin::general.dashboard')->with([
+            'data' => $data
+        ]);
     }
 }
--- app/Modules/Admin/Resources/Views/general/dashboard.blade.php
+++ app/Modules/Admin/Resources/Views/general/dashboard.blade.php
@@ -1,5 +1,9 @@
 @extends('admin::layouts/master')
 
+@section('head')
+    {{ Html::style('assets/vendor/chartist/css/chartist-custom.css') }}    
+@endsection
+
 @section('content')
 	<div class="main">
 		<!-- MAIN CONTENT -->
@@ -14,13 +18,84 @@
 							</div>
 							<hr>
 							<div class="panel-body">
-								
+								<div class="row">
+                                    <div class="col-md-4">
+                                        <div class="metric">
+                                            <span class="icon"><i class="fa fa-download"></i></span>
+                                            <p>
+                                                <span class="number">{{ $data['count']['products'] }}</span>
+                                                <span class="title">Products</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="metric">
+                                            <span class="icon"><i class="fa fa-shopping-bag"></i></span>
+                                            <p>
+                                                <span class="number">{{ $data['count']['purchasing'] }}</span>
+                                                <span class="title">Sales</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="metric">
+                                            <span class="icon"><i class="fa fa-users"></i></span>
+                                            <p>
+                                                <span class="number">{{ $data['count']['users'] }}</span>
+                                                <span class="title">Users</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
 							</div>
 						</div>
 						<!-- END PANEL HEADLINE -->
-					</div>
+                    </div>
+                    <div class="col-md-12">
+                        <!-- VISIT CHART -->
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Sales Summary</h3>
+                                <div class="right">
+                                    <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
+                                    <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
+                                </div>
+                            </div>
+							<hr>                            
+                            <div class="panel-body">
+                                <div id="sales-chart" data-label="{{ $data['monthly']->pluck('month') }}" data-value="{{ $data['monthly']->pluck('count') }}" class="ct-chart"></div>
+                            </div>
+                        </div>
+                        <!-- END VISIT CHART -->
+                    </div>
 				</div>
 			</div>
 		</div>
 	</div>
 @endsection
+
+@push('scripts')
+    {{ Html::script('assets/vendor/chartist/js/chartist.min.js') }}       
+
+    <script>
+        // visits chart
+		data = {
+			labels: $('#sales-chart').data('label'),
+			series: [
+				$('#sales-chart').data('value')
+			]
+		};
+
+		options = {
+			height: 300,
+			axisX: {
+				showGrid: false
+			},
+            axisY: {
+                scaleMinSpace: 100
+            }
+		};
+
+		new Chartist.Bar('#sales-chart', data, options);
+    </script>
+@endpush
